765a4f3b4e77cfca958317673236e96247c2d7e7
Anonymous <>
2023-11-01 23:55:55 +0200

    chore: Document the DoubleBufferQueue class a bit more.

--- src/ccol/double_buffer_queue.h
+++ src/ccol/double_buffer_queue.h
@@ -10,6 +10,8 @@
 
 namespace ccol {
 
+/// \brief A double buffer queue is a queue class that allows multiple writers to add to a back buffer
+/// and multiple readers to access a front read-only buffer
 template <typename TElement>
 class DoubleBufferQueue final {
 
@@ -29,52 +31,67 @@ class DoubleBufferQueue final {
   using reverse_iterator = BufferType::reverse_iterator;
   using const_reverse_iterator = BufferType::const_reverse_iterator;
 
+  DoubleBufferQueue() = default;
+  DoubleBufferQueue(const DoubleBufferQueue&) = delete;
+  DoubleBufferQueue(DoubleBufferQueue&&) = delete;
+  DoubleBufferQueue& operator=(const DoubleBufferQueue&) = delete;
+  DoubleBufferQueue& operator=(DoubleBufferQueue&&) = delete;
+  ~DoubleBufferQueue() = default;
+
+  /// \brief Safely push back a value to the back buffer
   void push_back(TElementParam element);
+  /// \brief Check if back buffer has values before swapping
   bool is_back_buffer_empty() const;
+  /// \brief Safely swap buffers (will wait for readers and writers)
   void swap_buffers();
 
-  size_type size() const { return buffers_[active_buffer_.load()].size(); }
+  /// \brief Get the front buffer size
+  size_type size() const { return buffers_[front_buffer_.load()].size(); }
 
-  void lock() const { active_buffer_mutex_.lock_shared(); }
+  /// \brief Marks the front buffer as being read. You need to call unlock() when done.
+  /// \see DoubleBufferQueue::unlock()
+  void lock() const { front_buffer_mutex_.lock_shared(); }
 
-  void unlock() const { active_buffer_mutex_.unlock_shared(); }
+  /// \brief Unlocks the front buffer from reading.
+  void unlock() const { front_buffer_mutex_.unlock_shared(); }
 
-  [[nodiscard]] const_iterator begin() const { return buffers_[active_buffer_.load()].begin(); }
+  [[nodiscard]] const_iterator begin() const { return buffers_[front_buffer_.load()].begin(); }
 
-  [[nodiscard]] const_iterator end() const { return buffers_[active_buffer_.load()].end(); }
+  [[nodiscard]] const_iterator end() const { return buffers_[front_buffer_.load()].end(); }
 
-  [[nodiscard]] const_iterator rbegin() const { return buffers_[active_buffer_.load()].begin(); }
+  [[nodiscard]] const_iterator rbegin() const { return buffers_[front_buffer_.load()].begin(); }
 
-  [[nodiscard]] const_iterator rend() const { return buffers_[active_buffer_.load()].end(); }
+  [[nodiscard]] const_iterator rend() const { return buffers_[front_buffer_.load()].end(); }
 
-  TElementParam operator[](size_type index) const { return buffers_[active_buffer_.load()][index]; }
+  TElementParam operator[](size_type index) const { return buffers_[front_buffer_.load()][index]; }
 
-  bool empty() const { return buffers_[active_buffer_.load()].empty(); }
+  /// \brief Checks if the front buffer has any values.
+  bool empty() const { return buffers_[front_buffer_.load()].empty(); }
 
  private:
   std::array<BufferType, 2> buffers_;
   mutable std::mutex back_buffer_mutex_;
-  mutable std::shared_mutex active_buffer_mutex_;
-  std::atomic<std::uint8_t> active_buffer_;
+  mutable std::shared_mutex front_buffer_mutex_;
+  std::atomic<std::uint8_t> front_buffer_;
 };
 
 template <typename TElement>
 bool DoubleBufferQueue<TElement>::is_back_buffer_empty() const {
   std::lock_guard _scoped_lock(back_buffer_mutex_);
-  return buffers_[active_buffer_.load() ^ 1].empty();
+  return buffers_[front_buffer_.load() ^ 1].empty();
 }
 
 template <typename TElement>
 void DoubleBufferQueue<TElement>::swap_buffers() {
-  std::scoped_lock _scoped_lock(back_buffer_mutex_, active_buffer_mutex_);
-  const std::uint8_t last_active_buffer = active_buffer_.fetch_xor(1);
+  std::scoped_lock _scoped_lock(back_buffer_mutex_, front_buffer_mutex_);
+  const std::uint8_t last_active_buffer = front_buffer_.fetch_xor(1);
   buffers_[last_active_buffer].clear();
 }
 
 template <typename TElement>
 void DoubleBufferQueue<TElement>::push_back(TElementParam element) {
   std::lock_guard _scoped_lock(back_buffer_mutex_);
-  buffers_[active_buffer_.load() ^ 1].push_back(element);
+  buffers_[front_buffer_.load() ^ 1].push_back(element);
 }
 
 }  // namespace ccol
