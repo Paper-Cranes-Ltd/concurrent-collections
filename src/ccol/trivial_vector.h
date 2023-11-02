ee6411028489b52f518dc18089064dfd05b4678c
Anonymous <>
2023-11-02 18:13:38 +0200

    feat: Create a TrivialVector collection.
    This collection allows for trivial types to be stored and accessed safely. Reading has a very simple sync mechanism without an actual lock. Writing requires a lock to be acquired.

---
+++ src/ccol/trivial_vector.h
@@ -0,0 +1,178 @@
+//
+// Copyright (c) 2023 Paper Cranes Ltd.
+// All rights reserved.
+//
+
+#ifndef CONCURRENTCOLLECTIONS_CONCURRENT_POINTER_VECTOR_H_
+#define CONCURRENTCOLLECTIONS_CONCURRENT_POINTER_VECTOR_H_
+
+#include <ccol/common.h>
+
+namespace ccol {
+
+template <typename TElement>
+class TrivialVector {
+ public:
+  static_assert(std::is_trivially_copyable_v<TElement>, "Trivial vector can only contain trivial elements");
+  using value_type = TElement;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+
+  struct iterator {
+    using iterator_category = std::contiguous_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = TrivialVector::value_type;
+    using pointer = value_type;
+    using reference = value_type;
+
+    explicit iterator(const TrivialVector& tv_, TrivialVector::size_type index_)
+        : index(index_), tv(tv_){};
+
+    value_type operator*() const { return tv[index]; }
+
+    value_type operator->() { return tv[index]; }
+
+    iterator operator++(int) {
+      iterator tmp = *this;
+      index++;
+      return tmp;
+    }
+
+    iterator& operator++() {
+      ++index;
+      return *this;
+    }
+
+    iterator& operator+=(size_type amount) {
+      index += amount;
+      return *this;
+    }
+
+    iterator operator--(int) {
+      iterator tmp = *this;
+      index--;
+      return tmp;
+    }
+
+    iterator& operator--() {
+      --index;
+      return *this;
+    }
+
+    iterator& operator-=(size_type amount) {
+      index -= amount;
+      return *this;
+    }
+
+    bool equals(const iterator& other) const { return &tv == &other.tv && index == other.index; }
+
+    bool operator==(const iterator& other) const { return equals(other); };
+
+    bool operator!=(const iterator& other) const { return !equals(other); };
+
+    TrivialVector::size_type index = 0;
+    const TrivialVector& tv;
+  };
+
+  value_type operator[](size_type index) const;
+
+  size_type size() const { return size_.load(); }
+
+  void clear() { resize(0); }
+
+  void resize(size_type new_size);
+  void push_back(value_type new_value);
+  void replace(size_type index, value_type new_value);
+  [[nodiscard]] value_type exchange(size_type index, value_type new_value);
+
+  iterator begin() const { return iterator(*this, 0); }
+  iterator end() const { return iterator(*this, size()); }
+
+ private:
+  std::array<std::atomic<value_type*>, 2> safe_buffers_{nullptr, nullptr};
+  mutable std::array<std::atomic<std::uint32_t>, 2> reader_counters_{0, 0};
+  mutable std::recursive_mutex write_mutex_;
+  std::atomic<std::uint8_t> active_buffer_ = 0;
+  std::atomic<size_type> size_ = 0;
+  std::atomic<size_type> reserved_ = 0;
+
+  struct ScopedReader {
+    ScopedReader(std::atomic<std::uint32_t>& x)
+        : counter(x) {
+      ++counter;
+    }
+
+    ~ScopedReader() { --counter; }
+
+    std::atomic<std::uint32_t>& counter;
+  };
+};
+
+template <typename TElement>
+TrivialVector<TElement>::value_type TrivialVector<TElement>::exchange(size_type index, value_type new_value) {
+  std::lock_guard _scoped_lock(write_mutex_);
+  value_type old = safe_buffers_[active_buffer_.load()][index];
+  safe_buffers_[active_buffer_.load()][index] = new_value;
+  return old;
+}
+
+template <typename TElement>
+void TrivialVector<TElement>::replace(TrivialVector::size_type index, value_type new_value) {
+  std::lock_guard _scoped_lock(write_mutex_);
+  safe_buffers_[active_buffer_.load()][index] = new_value;
+}
+
+template <typename TElement>
+void TrivialVector<TElement>::push_back(value_type new_value) {
+  std::lock_guard _scoped_lock(write_mutex_);
+  resize(size() + 1);
+  safe_buffers_[active_buffer_.load()][size() - 1] = new_value;
+}
+
+template <typename TElement>
+TrivialVector<TElement>::value_type TrivialVector<TElement>::operator[](TrivialVector::size_type index) const {
+  ScopedReader reader(reader_counters_[active_buffer_.load()]);
+  std::uint8_t read_index = &reader_counters_[0] == &reader.counter ? 0 : 1;
+  return safe_buffers_[read_index][index];
+}
+
+template <typename TElement>
+void TrivialVector<TElement>::resize(TrivialVector::size_type new_size) {
+  std::lock_guard _scoped_lock(write_mutex_);
+
+  if (size() >= new_size) {
+    size_.store(new_size);
+    return;
+  }
+
+  if (reserved_.load() < new_size) {
+    size_type new_reserved = reserved_.load() == 0 ? 2 : reserved_.load() * 2;
+    auto* new_buffer = new value_type[new_reserved];
+    if (size() > 0) {
+      auto* current_active_buffer = safe_buffers_[active_buffer_.load()].load();
+      std::copy(current_active_buffer, current_active_buffer + size(), new_buffer);
+    }
+    std::fill(new_buffer + size(), new_buffer + new_size, TElement{});
+
+    // sanity check make sure back buffer is free
+    auto* old_buffer = safe_buffers_[active_buffer_.load() ^ 1].exchange(new_buffer);
+    std::uint8_t back_index_after_swap = active_buffer_.fetch_xor(1);
+    size_.store(new_size);
+    reserved_.store(new_reserved);
+    delete[] old_buffer;
+
+    // free back buffer after there are no readers
+    while (reader_counters_[back_index_after_swap].load() > 0) {
+      std::this_thread::yield();
+    }
+
+    old_buffer = safe_buffers_[back_index_after_swap].exchange(nullptr);
+    delete[] old_buffer;
+  } else {
+    size_.store(new_size);
+  }
+}
+
+}  // namespace ccol
+
+#endif  // CONCURRENTCOLLECTIONS_CONCURRENT_POINTER_VECTOR_H_
---
+++ tests/trivial_vector_test.cpp
@@ -0,0 +1,121 @@
+//
+// Copyright (c) 2023 Paper Cranes Ltd.
+// All rights reserved.
+//
+#include <catch2/catch_all.hpp>
+#include <ccol/trivial_vector.h>
+
+#include <barrier>
+
+struct TrivialPoint final {
+  TrivialPoint() = default;
+
+  TrivialPoint(std::float_t x_, std::float_t y_)
+      : x(x_), y(y_) {}
+
+  TrivialPoint(const TrivialPoint&) = default;
+  TrivialPoint& operator=(const TrivialPoint&) = default;
+
+  [[nodiscard]] bool equals(const TrivialPoint& other) const { return x == other.x && y == other.y; }
+
+  [[nodiscard]] bool operator==(const TrivialPoint& other) const { return equals(other); }
+
+  [[nodiscard]] bool operator!=(const TrivialPoint& other) const { return !equals(other); }
+
+  std::float_t x = 0.0f;
+  std::float_t y = 0.0f;
+};
+
+TEST_CASE("TrivialVector Basic Operations", "[tvector]") {
+  SECTION("Integer Elements") {
+    ccol::TrivialVector<std::uint32_t> elements;
+    elements.push_back(0);
+    elements.push_back(1);
+    elements.push_back(2);
+
+    CHECK(elements.size() == 3);  // NOLINT(*-container-size-empty)
+
+    for (std::uint32_t i = 0; i < elements.size(); i++) {
+      CHECK(elements[i] == i);
+    }
+
+    std::uint32_t ix = 0;
+    for (std::uint32_t element : elements) {
+      CHECK(element == ix++);
+    }
+  }
+
+  SECTION("Pointer Elements") {
+    std::array<std::uint32_t, 2> row{0, 1};
+    ccol::TrivialVector<std::uint32_t*> elements;
+    elements.push_back(row.data());
+
+    CHECK(elements.size() == 1);  // NOLINT(*-container-size-empty)
+
+    CHECK(elements[0] == row.data());
+    CHECK(elements[0][0] == 0);
+    CHECK(elements[0][1] == 1);
+  }
+
+  SECTION("Trivial Struct Elements") {
+    ccol::TrivialVector<TrivialPoint> elements;
+    elements.push_back({0.0f, 0.0f});
+    elements.push_back({0.0f, 1.0f});
+    elements.push_back({1.0f, 0.0f});
+    elements.push_back({1.0f, 1.0f});
+
+    CHECK(elements.size() == 4);  // NOLINT(*-container-size-empty)
+
+    CHECK(elements[0] == TrivialPoint{0.0f, 0.0f});
+  }
+}
+
+TEST_CASE("TrivialVector MT Access", "[tvector][!mayfail][!throws]") {
+  ccol::TrivialVector<std::uint32_t> elements;
+  std::barrier sync_point(2);
+
+  {
+    std::jthread push_thread([&elements, &sync_point]() {
+      sync_point.arrive_and_wait();
+      for (std::uint32_t i = 0; i < 256000; i++) {
+        elements.push_back(i);
+        std::this_thread::yield();
+      }
+    });
+
+    sync_point.arrive_and_wait();
+    std::this_thread::yield();
+
+    SECTION("Access Validation") {
+      CHECK(elements.size() > 0);
+      for (std::uint32_t i = 0; i < elements.size(); i++) {
+        CHECK(elements[i] == i);
+      }
+    }
+
+    SECTION("Benchmark MT", "") {
+      std::int32_t size = std::min<std::int32_t>(100, static_cast<std::int32_t>(elements.size()));
+      BENCHMARK("Single Random Element") {
+        return elements[size - 1];
+      };
+
+      BENCHMARK("Sum 0-100") {
+        std::uint32_t sum = 0;
+        for (std::uint32_t i = 0; i < size; i++) {
+          sum += elements[i];
+        }
+        return sum;
+      };
+    }
+  }
+
+  SECTION("Benchmark ST") {
+    BENCHMARK("Sum 0-100") {
+      std::uint32_t sum = 0;
+      for (std::uint32_t i = 0; i < 100; i++) {
+        sum += elements[i];
+      }
+      return sum;
+    };
+  }
+}
